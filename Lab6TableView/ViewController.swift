a447a6fe5c210b6487b36bd2d9e48c1c6a27e694
Mahesh Pareek <pareekm644@gmail.com>
2024-02-18 20:23:08 -0500

    Initial Commit

---
+++ Lab6TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab6TableView
+//
+//  Created by Mahesh Pareek on 2024-02-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
